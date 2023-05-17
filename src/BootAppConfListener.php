a5f7a7516a96b9be087ffe694e246621d6c820e4
Shuo <awesee@dingtalk.com>
2023-05-17 10:38:50 +0800

    fix: server

--- src/BootAppConfListener.php
+++ src/BootAppConfListener.php
@@ -44,9 +44,9 @@ class BootAppConfListener implements ListenerInterface
         $router = $container->get(DispatcherFactory::class)->getRouter('http');
         $data = $router->getData();
         $servers = $config->get('server.servers');
-        if (count($servers) > 1 && !Str::contains($output, '{server}')) {
-            $logger->warning('You have multiple serve, but your swagger.output_file not contains {server} var');
-        }
+        // if (count($servers) > 1 && !Str::contains($output, '{server}')) {
+        //     $logger->warning('You have multiple serve, but your swagger.output_file not contains {server} var');
+        // }
         foreach ($servers as $server) {
             $swagger = new ChatPluginsJson($server['name']);
             #跳过非http的服务
